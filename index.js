a631c62a9b05d7ded3ea95957695937f84673a6b
Bonnie Cruz <cruzbonnie@ymail.com>
2025-03-20 12:51:01 +0000

    Implemented ID's to new to do's + updated delete to do function to remove tasks from local storage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var myList = document.getElementById("todo-list");
 
 if (json_data) {
     json_data.forEach((element) => {
-        newTodo(element.title);
+        newTodo(element.title, element.id);
     });
 }
 
@@ -25,15 +25,20 @@ function registerEventListener() {
 function deleteTodo() {
     var li = this.parentElement; // Grabbing parent list element of the text and delete button which are direct children of the list
     myList.removeChild(li); // Removing the list element
+    var json_temp = JSON.parse(localStorage.getItem("json_data"));
+    //How do we delete the item from json_temp without a unique identifier?
+    delete json_temp[li.dataset.id];
+    localStorage.setItem("json_data", JSON.stringify(json_temp));
 }
 
-function newTodo(todoTitle) {
-    if (!todoTitle) {
+function newTodo(todoTitle, todoID) {
+    if (!todoTitle && !todoID) {
         // If textbox is not empty, execute the below code
         todoTitle = document.getElementById("todoTitle").value;
-        storeTodoLocal(todoTitle);
+        var todoID = storeTodoLocal(todoTitle);
     }
     var listItem = document.createElement("li");
+    listItem.dataset.id = todoID;
     listItem.appendChild(document.createTextNode(todoTitle));
 
     var deleteLink = document.createElement("a");
@@ -58,8 +63,12 @@ function storeTodoLocal(todoTitle) {
     }
     //If json_temp is null, (i.e. no previous data was found), it initialises an empty array. This ensures that we have a valid array to work with.
 
+    //Creating a new to do ID based on length of existing local storage array
+    var todoID = json_temp.length;
+
     // add new todo object to JSON/JS array created above
     json_temp.push({
+        id: todoID,
         title: todoTitle,
         completed: false,
     });
@@ -70,4 +79,5 @@ function storeTodoLocal(todoTitle) {
     // stringify updated JSON and store back in localStorage
     localStorage.setItem("json_data", JSON.stringify(json_temp));
     // Converts the updated json_temp array back into a JSON string
+    return todoID;
 }
