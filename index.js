0405c547b6420ab22268277e5710e5b41c2dcb0c
Bonnie Cruz <cruzbonnie@ymail.com>
2025-03-20 11:56:46 +0000

    creating function to store to do tasks locally

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var json_data = [];
+/*var json_data = [
+    {
+        title: "sample todo item",
+        completed: false,
+    },
+];*/
+
+var json_data = JSON.parse(localStorage.getItem("json_data"));
 var myList = document.getElementById("todo-list");
 
-json_data.forEach((element) => {
-    newTodo(element.title);
-});
+if (json_data) {
+    json_data.forEach((element) => {
+        newTodo(element.title);
+    });
+}
 
 registerEventListener();
 function registerEventListener() {
@@ -22,6 +31,7 @@ function newTodo(todoTitle) {
     if (!todoTitle) {
         // If textbox is not empty, execute the below code
         todoTitle = document.getElementById("todoTitle").value;
+        storeTodoLocal(todoTitle);
     }
     var listItem = document.createElement("li");
     listItem.appendChild(document.createTextNode(todoTitle));
@@ -35,4 +45,29 @@ function newTodo(todoTitle) {
     registerEventListener();
     console.log("new todo added");
     console.log(listItem);
+}
+
+//This function takes todoTitle as an argument, which represents the text of the new to-do item
+function storeTodoLocal(todoTitle) {
+    // retrieve and parse existing JSON from localstorage
+    var json_temp = JSON.parse(localStorage.getItem("json_data"));
+    //Retrieve existing list of to-do items stored in local storage
+
+    if (!json_temp) {
+        json_temp = [];
+    }
+    //If json_temp is null, (i.e. no previous data was found), it initialises an empty array. This ensures that we have a valid array to work with.
+
+    // add new todo object to JSON/JS array created above
+    json_temp.push({
+        title: todoTitle,
+        completed: false,
+    });
+
+    // log updated JSON to console
+    console.log(json_temp);
+
+    // stringify updated JSON and store back in localStorage
+    localStorage.setItem("json_data", JSON.stringify(json_temp));
+    // Converts the updated json_temp array back into a JSON string
 }
